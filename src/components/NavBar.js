dedd6ffd98ecb1ddf419f88b6f33f22ce18c9bc8
Almonzo Rhoden <almonzo.rhoden@gmail.com>
2021-02-02 22:50:40 -0500

    uploading files

---
+++ src/components/NavBar.js
@@ -0,0 +1,66 @@
+// Including program components
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+import  "../stylesheets/NavBar.css"
+
+// Re-usebale code, Navbar component
+function NavBar() {
+  // This allows the component to check the route any time the user uses a link to navigate.
+  const location = useLocation();
+
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      {/* Adding eslint to make code cleaner while incorporating bootstrap*/}
+     {/* eslint-disable-next-line */}
+      <a className="navbar-brand justify-content-end ">Almonzo Rhoden</a>
+
+      {/* Navbar button for mobile phones */}
+      <button className="navbar-toggler justify-content-end" type="button" data-toggle="collapse"
+        data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false"
+        aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+      <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
+        <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+          <li className="nav-item">
+
+            {/* Home Nav link code */}
+            <a className="nav-link">
+              <Link to="/" className={location.pathname === "/" ? "nav-link active" : "nav-link"}>
+                About
+              </Link><span className="sr-only">(current)</span>
+            </a>
+          </li>
+
+          {/* Nav Portfolio link  */}
+          <li className="nav-item">
+            <a className="nav-link">
+              <Link
+                to="/portfolio"
+                className={location.pathname === "/portfolio" ? "nav-link active" : "nav-link"}
+              >
+                Portfolio
+      </Link>
+            </a>
+          </li>
+
+          {/* Nav Contact link  */}
+          <li className="nav-item">
+            <a className="nav-link" href="./assets/html/contact.html">
+              <Link
+                to="/contact"
+                className={location.pathname === "/contact" ? "nav-link active" : "nav-link"}
+              >
+                Contact
+        </Link>
+            </a>
+          </li>
+        </ul>
+
+      </div>
+    </nav>
+  );
+}
+
+export default NavBar;
---
+++ src/pages/About.js
@@ -0,0 +1,78 @@
+// Including program components
+import React from "react";
+import "../stylesheets/About.css"
+
+// Re-usable code, Bootstrap jumbotron
+function About() 
+{
+  return (
+    <div>
+      
+      {/* Bootstrap jumbotron start*/}
+      <div className="jumbotron jumbotron-fluid" style = {{borderRadius:"20px"}}>
+      <div className="container">
+        <h1 className="display-5">About Me</h1>
+        <hr className="my-4"/>
+        {/*eslint-disable-next-line */}
+        <img id="almonzo-img" src="./assets/images/almonzo-img.png" alt="almonzo-image"/>
+       
+       {/* Spacing */}
+        <br/> <br/>
+  
+        {/* About me section*/}
+        <p>
+          Hi! Welcome to my page! I'm a Front-end web developer based in North Jersey leveraging an Information Systems degree from Rutgers University and
+          customer service background to build quality user experiences in mobile and web applications. <br/> <br/>
+
+          Recently earned a
+          certificate in full stack development from Rutgers University, enhancing my skills in responsive web design,
+          Bootstrap, JavaScript, API, Express, React, Node, and MySQL. Passionate about creating simplistic
+          life-enhancing applications and collaborating with others to develop
+          meaningful innovative experiences. <br/> <br/>
+
+          My previous work in Information Technology over the years, combined with an
+          authentic customer service approach, will allow me to provide unique ideas on how people interact with websites
+          and software platforms. Feel free to check out my <a href= "./assets/html/portfolio.html">portfolio</a> or <a href= "./assets/html/contact.html">contact</a> me to learn more thank you.   <br/>
+        
+        <br/>
+          
+          {/* Calming background music */}
+          <p>False Astronomy ♫</p>
+          <audio controls loop id="myaudio">
+            <source src="./assets/background/deep_space2.mp3" type="audio/mpeg"/>
+          </audio>
+        </p>
+       
+      </div>
+
+    </div>
+
+    {/* Resume section */}
+    <div className="card border-dark mb-1" style={{maxWidth: "80rem", maxHeight: "40rem"}}>
+      <div className="card-header"><strong>Let's connect</strong></div>
+      <div className="contact-body text-dark">
+        <br/>
+      
+        <p><a href="mailto: almonzo.rhoden@gmail.com"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-envelope" viewBox="0 0 16 16">
+          <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2zm13 2.383l-4.758 2.855L15 11.114v-5.73zm-.034 6.878L9.271 8.82 8 9.583 6.728 8.82l-5.694 3.44A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.739zM1 11.114l4.758-2.876L1 5.383v5.73z"/>
+        </svg> AlmonzoRhoden@gmail.com</a></p>
+
+      
+        <p> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-telephone" viewBox="0 0 16 16">
+          <path d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.568 17.568 0 0 0 4.168 6.608 17.569 17.569 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.678.678 0 0 0-.58-.122l-2.19.547a1.745 1.745 0 0 1-1.657-.459L5.482 8.062a1.745 1.745 0 0 1-.46-1.657l.548-2.19a.678.678 0 0 0-.122-.58L3.654 1.328zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"/>
+        </svg> 862-438-2135</p>
+
+        {/*eslint-disable-next-line */}
+        <iframe src="./assets/images/Rhoden_Resume.pdf" width="100%" height="300px"></iframe>
+        <strong><p>If your browser does not support this PDF, please click here to download: <a href="./assets/images/Rhoden_Resume.pdf">Download PDF</a></p></strong>
+      </div>
+    </div>
+
+<br/>
+
+    </div>
+
+  );
+}
+
+export default About;

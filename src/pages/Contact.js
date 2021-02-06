efe94a527a4ad7807856200aaf6b03aa2417dde9
Almonzo Rhoden <almonzo.rhoden@gmail.com>
2021-02-05 19:22:24 -0500

    updating code

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,55 +3,55 @@ import { Link, Route } from "react-router-dom";
 
 import logo from "../images/gif-1.gif"
 import Portfolio from "./Portfolio";
+import Mailto from var Mailto = require('react-mailto');
 
-function Contact(props) 
-{
+function Contact(props) {
   return (
     <div>
-      
 
-      {/* <div className="jumbotron">
-      <h1 className="display-4">Contact</h1>
-      <p className="lead"> </p>
-      <hr className="my-4"/>
-      <p>
 
-      <main>
+      <div className="jumbotron">
+        <h1 className="display-4">Contact</h1>
+        <p className="lead"> </p>
+        <hr className="my-4" />
         
-        <form>
-          <div className="form-group">
-            
-            <label for="exampleInputName1"> Name</label>
-            <input type="name" className="form-control" id="exampleInputName1" aria-describedby="emailHelp"
-               placeholder= " 👤 Enter Name" required/>
-          </div>
 
-          <div className="form-group">
-            <label for="exampleInputEmail1">Email</label>
-            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
-              placeholder=" ✉️ Enter Email" required/>
-            <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone
+          <main>
+
+            <form>
+              <div className="form-group">
+
+                <label for="exampleInputName1"> Name</label>
+                <input type="name" className="form-control" id="exampleInputName1" aria-describedby="emailHelp"
+                  placeholder=" 👤 Enter Name" required />
+              </div>
+
+              <div className="form-group">
+                <label for="exampleInputEmail1">Email</label>
+                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+                  placeholder=" ✉️ Enter Email" required />
+                <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone
               else.</small>
-          </div>
+              </div>
 
-          <div className="form-group">
-            <label for="exampleInputPassword1">Message</label>
-            <input type="message" className="form-control-1" id="exampleInputMessage1" placeholder=" 🙂 Message me..." required/>
-          </div>
+              <div className="form-group">
+                <label for="exampleInputPassword1">Message</label>
+                <input type="message" className="form-control-1" id="exampleInputMessage1" placeholder=" 🙂 Message me..." required />
+              </div>
 
-          <div className="form-check">
-            <button type="submit" className="btn btn-primary" id="submitBtn"><a
-                href="https://www.linkedin.com/in/almonzorhoden/"></a>Submit</button>
+              <div className="form-check">
+              <a href={"mailto:almonzorhoden@gmail.com"} target="_blank" rel="noopener noreferrer"><button type="submit" className="btn btn-primary" id="submitBtn">Submit</button></a>
+              </div>
             </form>
 
-      </main> <br/>
+          </main> <br />
 
-       <button id="GitBtn" className="btn btn-dark" button type="submit">Visit Github</button> <button id="LinkedInBtn" className="btn btn-dark" button type="submit">Visit LinkedIn</button>
-    </div> */}
-
-    <img src={logo} alt="Logo" style={{margin:"auto"}}/>
-
+          <button id="GitBtn" className="btn btn-dark" button type="submit">Visit Github</button> <button id="LinkedInBtn" className="btn btn-dark" button type="submit">Visit LinkedIn</button>
     </div>
+
+        <img src={logo} alt="Logo" style={{ margin: "auto" }} />
+
+      </div>
   );
 }
 
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -17,7 +17,7 @@ function Portfolio(props)
       <h2 class="glow">Covid-19 App</h2>
       <div class="Box-1"></div> <br/>
       <p>An app that helps you get tested for Covid-19.</p>
-      <button id="testingBtn" class="btn btn-dark" button type="submit">Visit Page</button>
+      <a href = 'https://almonzorhoden.github.io/covid-19-app/'target = '_blank' rel = 'noreferrer'><button id="testingBtn" class="btn btn-dark" button type="submit">Visit Page</button></a>
       <button id="testingGitBtn" class="btn btn-dark" button type="submit">Visit Github</button>
     </div>
 
@@ -26,7 +26,7 @@ function Portfolio(props)
       <h2 class="display-5">Book Book</h2>
       <div class="Box-2"></div> <br/>
       <p> An app that let's you read and checkout books online.</p>
-      <button id="BookBtn" class="btn btn-dark" button type="submit">Visit Page</button>
+      <a><button id="BookBtn" class="btn btn-dark" button type="submit">Visit Page</button></a>
       <button id="BookGitBtn" class="btn btn-dark" button type="submit">Visit Github</button>
     </div>
 
@@ -65,41 +65,6 @@ function Portfolio(props)
       <button id="fitnessBtn" class="btn btn-dark" button type="submit">Visit Page</button>
       <button id="fitnessGitBtn" class="btn btn-dark" button type="submit">Visit Github</button>
     </div>
-
-    <div class="dropdown" styles="text-align:center">
-  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-    Favorite Projects
-  </button>
-  <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
-    <button class="dropdown-item" type="button">
-      {/*  Box-1 */}
-    <div class="jumbotron">
-      <h2 class="glow">Covid-19 App</h2>
-      <div class="Box-1"></div> <br/>
-      <button id="testingBtn" class="btn btn-dark" button type="submit">Visit Page</button>
-      <button id="testingGitBtn" class="btn btn-dark" button type="submit">Visit Github</button>
-    </div>
-
-    {/* Box-2 */}
-    <div class="jumbotron">
-      <h2 class="display-5">Book Book</h2>
-      <div class="Box-2"></div> <br/>
-     
-      <button id="BookBtn" class="btn btn-dark" button type="submit">Visit Page</button>
-      <button id="BookGitBtn" class="btn btn-dark" button type="submit">Visit Github</button>
-    </div>
-
-    {/* Box-3 */}
-    <div class="jumbotron" id='align-jumbo'>
-      <h2 class="display-5">Weather-Board</h2>
-      <div class="Box-3"></div> <br/>
-      <button id="weatherBtn" class="btn btn-dark" button type="submit">Visit Page</button>
-      <button id="weatherGitBtn" class="btn btn-dark" button type="submit">Visit Github</button>
-    </div>
-    </button>
-
-  </div>
-</div>
 
     </div>
 

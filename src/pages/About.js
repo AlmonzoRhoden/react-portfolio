4c62c3e7791b4511098462b3336f10ee579f41c0
Almonzo Rhoden <almonzo.rhoden@gmail.com>
2021-03-08 08:32:34 -0500

    Updating Site

--- src/pages/About.js
+++ src/pages/About.js
@@ -23,7 +23,7 @@ function About() {
 
           {/* About me section*/}
           <p>
-            Hi! Welcome to my page! I'm a <u>Front-End Developer</u> based in North Jersey leveraging an Information Systems degree from Rutgers University and
+            Hi! Welcome to my page! I'm a Front-End Developer based in North Jersey leveraging an Information Systems degree from Rutgers University and
     customer service background to build quality user experiences in mobile and web applications. <br /> <br />
 
             <strong>Recently earned a
@@ -87,7 +87,7 @@ function About() {
 
           <p> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-telephone" viewBox="0 0 16 16">
             <path d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.568 17.568 0 0 0 4.168 6.608 17.569 17.569 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.678.678 0 0 0-.58-.122l-2.19.547a1.745 1.745 0 0 1-1.657-.459L5.482 8.062a1.745 1.745 0 0 1-.46-1.657l.548-2.19a.678.678 0 0 0-.122-.58L3.654 1.328zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z" />
-          </svg> 862-438-2135</p>
+          </svg> <a href={"tel:8624382135"}>862-438-2135</a></p>
 
           {/*eslint-disable-next-line */}
           <iframe src={resume} width="100%" height="300px" ></iframe>
